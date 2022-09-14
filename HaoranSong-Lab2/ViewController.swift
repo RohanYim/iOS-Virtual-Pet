69cd2008f79891d595e5e092f0076589ffdfa158
Rohan <heronsong1024@gmail.com>
2022-09-14 10:58:38 -0500

    Initial Commit

---
+++ HaoranSong-Lab2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaoranSong-Lab2
+//
+//  Created by Haoran Song on 9/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
